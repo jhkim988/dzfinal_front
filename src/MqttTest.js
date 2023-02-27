6290ba2cc3e70ddc07658b68987616c526276ca0
jhkim <jinhankim988@gmail.com>
2023-02-28 08:58:14 +0900

    [Chore] Mqtt Test 설정

--- src/MqttTest.js
+++ src/MqttTest.js
@@ -1,26 +1,39 @@
-import { useState } from 'react';
-import mqtt from 'mqtt';
+import { useState, useEffect } from "react";
+import mqtt from "mqtt";
 
 const MqttTest = () => {
-    const options = {
-        clean: true,
-        connectTimeout: 4000,
-        clientId: 'emqx_test',
-        username: 'emqx_test',
-        password: 'emqx_test',
-      }
-      const [messages, setMessage] = useState('');
-      const client = mqtt.connect(`tcp://localhost:1883`, options);
-      client.on('message', (topic, payload, packet) => {
-        setMessage(messages.contat(payload.toString()))
-      })
-      const publish = () => {
-        client.publish('test', 'Hello, mqtt');
-      }
-    return <>
-        <button onClick={publish}>publish</button>
-        <h1>${messages}</h1>
+  const options = {
+    clean: true,
+    connectTimeout: 4000,
+    clientId: `emqx_test${Math.random()}`,
+    username: `emqx_test${Math.random()}`,
+    password: "emqx_test",
+    will: {
+      topic: "test",
+    },
+  };
+  const [input, setInput] = useState("");
+  const [client, setClient] = useState(
+    mqtt.connect(`ws://192.168.0.132:8083/mqtt`, options)
+  );
+  const [messages, setMessage] = useState("");
+  const publish = (val) => {
+    client.publish("test", val);
+  };
+  useEffect(() => {
+    client.on("message", (topic, payload, packet) => {
+      setMessage((prev) => payload.toString());
+    });
+  }, [client]);
+  client.subscribe("test");
+
+  return (
+    <>
+      <input onChange={(e) => setInput(e.target.value)} value={input} />
+      <button onClick={() => publish(input)}>publish</button>
+      <h1>${messages}</h1>
     </>
-}
+  );
+};
 
 export default MqttTest;
