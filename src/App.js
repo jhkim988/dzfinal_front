c5a22b62c69715c73ef48f9bca43cd71a6bf80a7
hyeeebin <knwsome@gmail.com>
2023-03-14 16:52:11 +0900

    [Feat]접수 진행중
    접수 진행중

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import Echo from './Echo';
+import { Grid } from '@mui/material';
+import DailyReservationList from './components/DailyReservationList';
+import Reception from './components/Reception';
+import ReceptionList from './components/ReceptionList';
 
 function App() {
   return (
-    <Echo/>
+    <>
+      <DailyReservationList />
+      <Reception />
+      <ReceptionList />
+    </>
   );
 }
 
---
+++ src/components/DailyReservationList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DailyReservationList = () => {
+    return (
+        <div>
+            <div>
+                <h2>당일 예약 현황</h2>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default DailyReservationList;
