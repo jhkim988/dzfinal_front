d491b0dcacf0b0f0bea948db81b89d65e0f69059
jhkim <jinhankim988@gmail.com>
2023-03-28 21:25:52 +0900

    [Feat] 대기열 - 수납이력 동기화

--- src/component/waiting/WaitingQueueLayout.js
+++ src/component/waiting/WaitingQueueLayout.js
@@ -69,23 +69,6 @@ const WaitingQueueLayout = ({ initPanel, nextState, clickRowCallback }) => {
   }
   // init data load
   useEffect(() => {
-    // setData([
-    //   { reception_id: '1', patient_id: '1', patient_name: "김진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "진료중" },
-    //   { reception_id: '2', patient_id: '2', patient_name: "이진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "진료대기" },
-    //   { reception_id: '3', patient_id: '3', patient_name: "박진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "진료대기" },
-    //   { reception_id: '4', patient_id: '4', patient_name: "최진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "진료대기" },
-    //   { reception_id: '5', patient_id: '5', patient_name: "남궁진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "진료대기" },
-    //   { reception_id: '6', patient_id: '6', patient_name: "제갈진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "수납중" },
-    //   { reception_id: '7', patient_id: '7', patient_name: "선우진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "수납대기" },
-    //   { reception_id: '8', patient_id: '8', patient_name: "정진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "수납대기" },
-    //   { reception_id: '9', patient_id: '9', patient_name: "백진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "수납대기" },
-    //   { reception_id: '10', patient_id: '10', patient_name: "남궁진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "수납대기" },
-    //   { reception_id: '11', patient_id: '11', patient_name: "김진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "수납완료" },
-    //   { reception_id: '12', patient_id: '12', patient_name: "진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "수납완료" },
-    //   { reception_id: '13', patient_id: '13', patient_name: "박진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "수납완료" },
-    //   { reception_id: '14', patient_id: '14', patient_name: "진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "수납완료" },
-    //   { reception_id: '15', patient_id: '15', patient_name: "남궁진한", front_registration_number: "961119", doctor_id: "1", doctor_name: "김더존", state: "수납완료" },
-    // ]);
     axios.get("/api/reception/today")
       .then(({ data }) => {
         setData(data);
