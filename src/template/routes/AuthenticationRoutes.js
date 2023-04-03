04208a951d6244442811a33e986a38f59d3149fd
joo <wjdwn3132@naver.com>
2023-04-03 21:35:17 +0900

    [Feat]  MinimalLayout

---
+++ src/template/routes/AuthenticationRoutes.js
@@ -0,0 +1,22 @@
+import { lazy } from 'react';
+
+// project imports
+import Loadable from '../ui-component/Loadable';
+import MinimalLayout from '../layout/MinimalLayout';
+
+const Login = Loadable(lazy(() => import('../../component/login/Login')));
+
+// ==============================|| AUTHENTICATION ROUTING ||============================== //
+
+const AuthenticationRoutes = {
+    path: '/',
+    element: <MinimalLayout />,
+    children: [
+        {
+            path: '/login',
+            element: <Login />
+        }
+    ]
+};
+
+export default AuthenticationRoutes;
