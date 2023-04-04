fa51bfd861436680cc839e0a84ba10be417ea028
Yoong <rhshfo8282@gmail.com>
2023-04-04 19:43:35 +0900

    [Feat] DID 대기자 화면 출력 구현

--- src/component/did/DidWaiting.js
+++ src/component/did/DidWaiting.js
@@ -1,35 +1,15 @@
 import { Box, Grid, OutlinedInput, Paper, Typography } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
-import React, { useState } from "react";
+import React from "react";
 
 
 
 
 
-const DidView = ({ initPanel, data }) => {
+const DidView = ({ data }) => {
   function MyFormHelperText() {
-    // const { focused } = useFormControl() || {};
-    // const helperText = React.useMemo(() => {
-    //   if (focused) {
-    //     return 'This field is being focused';
-    //   }
-    //   return 'Helper text';
-    // }, [focused]);
-    // return <FormHelperText>{helperText}</FormHelperText>;
+
   }
-
-//   const [value, setValue] = useState();
-
-//   const tabInfo = [
-//     { label: "내원", value: "1", filter: (data) => data.state !== "수납완료" },
-//     { label: "진료", value: "2", filter: (data) => data.state === "진료대기" || data.state === "진료중" },
-//     { label: "수납", value: "3", filter: (data) => data.state === "수납대기" || data.state === "수납중" },
-//     { label: "완료", value: "4", filter: (data) => data.state === "수납완료" },
-//   ];
-
-//   const handleChange = (event, newValue) => {
-//     setValue(newValue);
-//   };
 
 // 진료실1 호출할 사람들을 담음
 const waitingPatients = data.filter((item) => item.state === "진료대기" && item.doctor_id === 1 && item.state !== "진료중");
@@ -41,13 +21,51 @@ const waitingPatientForms = [...Array(Math.max(5 - waitingPatients.length, 0))].
         key={waitingPatients.length + index}
     >
         <OutlinedInput
-            placeholder={`진료실 ${waitingPatients.length + index + 1} 대기자`}
+            placeholder={`1 진료실 대기자`}
             inputProps={{ style: { textAlign: "center" } }}
             readOnly
         />
         <MyFormHelperText />
     </FormControl>
 ));
+
+// 진료실2 호출할 사람들을 담음
+const waitingPatients2 = data.filter((item) => item.state === "진료대기" && item.doctor_id === 2 && item.state !== "진료중");
+const waitingPatientForms2 = [...Array(Math.max(5 - waitingPatients2.length, 0))].map((_, index) => (
+    <FormControl
+        sx={{
+            width: "33%",
+        }}
+        key={waitingPatients2.length + index}
+    >
+        <OutlinedInput
+            placeholder={`2 진료실 대기자`}
+            inputProps={{ style: { textAlign: "center" } }}
+            readOnly
+        />
+        <MyFormHelperText />
+    </FormControl>
+));
+
+
+// 수납 호출할 사람들을 담음
+const waitingReceipt = data.filter((item) => item.state === "수납대기" && item.state !== "수납중");
+const waitingReceiptForms = [...Array(Math.max(5 - waitingReceipt.length, 0))].map((_, index) => (
+    <FormControl
+        sx={{
+            width: "33%",
+        }}
+        key={waitingReceipt.length + index}
+    >
+        <OutlinedInput
+            placeholder={`수납 대기자`}
+            inputProps={{ style: { textAlign: "center" } }}
+            readOnly
+        />
+        <MyFormHelperText />
+    </FormControl>
+));
+
 
 
   return (
@@ -74,8 +92,10 @@ const waitingPatientForms = [...Array(Math.max(5 - waitingPatients.length, 0))].
                 >
                 <Box sx={{ flexGrow: 1 }}>
                 <Typography variant="h2">
-                    진료실 1 / 담당의사: 김더존<br/>
+                    1 진료실  |  담당의사: 김더존<br/>
                 </Typography>
+                { }
+
                 <Box component="form" noValidate autoComplete="off">
                 {data.filter((item) => item.state === "진료중" && item.doctor_id === 1).length > 0 ? (
                 data.filter((item) => item.state === "진료중" && item.doctor_id === 1).map((item, index) => (
@@ -110,7 +130,7 @@ const waitingPatientForms = [...Array(Math.max(5 - waitingPatients.length, 0))].
                 </FormControl>
                 )}
 
-                {waitingPatients.map((item, index) => (
+                {waitingPatients.slice(0, 5).map((item, index) => (
                     <FormControl
                         sx={{
                             width: "33%",
@@ -118,7 +138,7 @@ const waitingPatientForms = [...Array(Math.max(5 - waitingPatients.length, 0))].
                         key={index}
                     >
                         <OutlinedInput
-                            placeholder={`진료실 ${index + 1} 대기자`}
+                            placeholder={"1 진료실 대기자"}
                             value={`${item.patient_name}(${item.front_registration_number})`}
                             inputProps={{ style: { textAlign: "center" } }}
                             readOnly
@@ -130,9 +150,9 @@ const waitingPatientForms = [...Array(Math.max(5 - waitingPatients.length, 0))].
                 <FormControl sx={{ width: "33%" }}>
                     <OutlinedInput
                         placeholder={
-                            waitingPatients.length <= 6
+                            waitingPatients.length <= 5
                                 ? `외 0명`
-                                : `외 ${waitingPatients.length - 6}명`
+                                : `외 ${waitingPatients.length - 5}명`
                         }
                         inputProps={{ style: { textAlign: "center" } }}
                         readOnly
@@ -141,70 +161,6 @@ const waitingPatientForms = [...Array(Math.max(5 - waitingPatients.length, 0))].
                 </FormControl>
 
                 </Box>
-                    {/* <Box component="form" noValidate autoComplete="off">
-                    <FormControl sx={{ width: "100%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 1 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 1 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 1 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 1 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 1 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 1 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="외 N명"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-                    </Box> */}
                 </Box>
                 </Paper>
             </Grid>
@@ -224,73 +180,74 @@ const waitingPatientForms = [...Array(Math.max(5 - waitingPatients.length, 0))].
                 >
                 <Box sx={{ flexGrow: 1 }}>
                     <Typography variant="h2">
-                    진료실 2 / 담당의사: 이을지
-                    <br/>
+                    2 진료실  |  담당의사: 이을지
                     </Typography>
+                    { }
 
                     <Box component="form" noValidate autoComplete="off">
-                    <FormControl sx={{ width: "100%" }}>
+                    {data.filter((item) => item.state === "진료중" && item.doctor_id === 2).length > 0 ? (
+                    data.filter((item) => item.state === "진료중" && item.doctor_id === 2).map((item, index) => (
+                        <FormControl
+                        sx={{
+                            width: "100%",
+                        }}
+                        key={index}
+                        >
                         <OutlinedInput
-                        placeholder="진료실 2 대기자"
+                            placeholder={`진료중 ${index + 1}`}
+                            value={`${item.patient_name}(${item.front_registration_number})`}
+                            inputProps={{ style: { textAlign: "center" } }}
+                            readOnly
+                        />
+                        <MyFormHelperText />
+                        </FormControl>
+                    ))
+                    ) : (
+                    <FormControl
+                        sx={{
+                        width: "100%",
+                        }}
+                    >
+                        <OutlinedInput
+                        placeholder={`진료중`}
+                        value={"잠시만 기다려주세용 ^*^"}
                         inputProps={{ style: { textAlign: "center" } }}
                         readOnly
+                        />
+                        <MyFormHelperText />
+                    </FormControl>
+                    )}
+
+                    {waitingPatients2.slice(0, 5).map((item, index) => (
+                        <FormControl
+                            sx={{
+                                width: "33%",
+                            }}
+                            key={index}
+                        >
+                            <OutlinedInput
+                                placeholder={"1 진료실 대기자"}
+                                value={`${item.patient_name}(${item.front_registration_number})`}
+                                inputProps={{ style: { textAlign: "center" } }}
+                                readOnly
+                            />
+                            <MyFormHelperText />
+                        </FormControl>
+                    ))}
+                    {waitingPatientForms2}
+                    <FormControl sx={{ width: "33%" }}>
+                        <OutlinedInput
+                            placeholder={
+                                waitingPatients2.length <= 5
+                                    ? `외 0명`
+                                    : `외 ${waitingPatients2.length - 5}명`
+                            }
+                            inputProps={{ style: { textAlign: "center" } }}
+                            readOnly
                         />
                         <MyFormHelperText />
                     </FormControl>
 
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 2 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 2 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 2 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 2 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="진료실 2 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="외 N명"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
                     </Box>
                 </Box>
                 </Paper>
@@ -314,71 +271,74 @@ const waitingPatientForms = [...Array(Math.max(5 - waitingPatients.length, 0))].
                     수납
                     <br/>
                     </Typography>
+                    { }
 
+                        {/* 수납대기: {waitingReceipt.length} 명 */}
                     <Box component="form" noValidate autoComplete="off">
-                    <FormControl sx={{ width: "100%" }}>
+                    {data.filter((item) => item.state === "수납중").length > 0 ? (
+                    data.filter((item) => item.state === "수납중").map((item, index) => (
+                        <FormControl
+                        sx={{
+                            width: "100%",
+                        }}
+                        key={index}
+                        >
                         <OutlinedInput
-                        placeholder="수납 대기자"
+                            value={`${item.patient_name}(${item.front_registration_number})`}
+                            inputProps={{ style: { textAlign: "center" } }}
+                            readOnly
+                        />
+                        <MyFormHelperText />
+                        </FormControl>
+                    ))
+                    ) : (
+                    <FormControl
+                        sx={{
+                        width: "100%",
+                        }}
+                    >
+                        <OutlinedInput
+                        placeholder={`진료중`}
+                        value={"잠시만 기다려주세용 ^*^"}
                         inputProps={{ style: { textAlign: "center" } }}
                         readOnly
+                        />
+                        <MyFormHelperText />
+                    </FormControl>
+                    )}
+                    {waitingReceipt.slice(0, 5).map((item, index) => (
+                        <FormControl
+                            sx={{
+                            width: "33%",
+                            }}
+                            key={index}
+                        >
+                            <OutlinedInput
+                            placeholder={"1 진료실 대기자"}
+                            value={`${item.patient_name}(${item.front_registration_number})`}
+                            inputProps={{ style: { textAlign: "center" } }}
+                            readOnly
+                            />
+                            <MyFormHelperText />
+                        </FormControl>
+                    ))}
+
+                    {waitingReceiptForms}
+                    <FormControl sx={{ width: "33%" }}>
+                        <OutlinedInput
+                            placeholder={
+                                waitingReceipt.length <= 5
+                                    ? `외 0명`
+                                    : `외 ${waitingReceipt.length - 5}명`
+                            }
+                            inputProps={{ style: { textAlign: "center" } }}
+                            readOnly
                         />
                         <MyFormHelperText />
                     </FormControl>
 
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="수납 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="수납 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="수납 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="수납 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="수납 대기자"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
-
-                    <FormControl sx={{ width: "33%" }}>
-                        <OutlinedInput
-                        placeholder="외 N명"
-                        inputProps={{ style: { textAlign: "center" } }}
-                        readOnly
-                        />
-                        <MyFormHelperText />
-                    </FormControl>
                     </Box>
+                    
                 </Box>
                 </Paper>
             </Grid>
