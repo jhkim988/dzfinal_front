2bb8e988f1562e1b1319bc5fec1e8046b7967aae
Yoong <rhshfo8282@gmail.com>
2023-04-06 10:03:51 +0900

    [Feat] ContextMqtt 적용

--- src/component/did/DID.js
+++ src/component/did/DID.js
@@ -1,27 +1,11 @@
-import { useRef, useEffect, useState, useCallback } from "react";
+import { useRef, useEffect, useState, useCallback, useContext } from "react";
 import DidSubscribe from "./DidSubscribe";
 import DidVideo from "./DidVideo";
 import DidWaiting from "./DidWaiting";
 import { Grid, Paper } from "@mui/material";
 import axios from "axios";
 import mqtt from "mqtt";
-
-
-// const mqttURL = `mqtt://192.168.0.132:8083/mqtt`;
-const mqttURL = `mqtt://localhost:8083/mqtt`;
-
-const genRanHex = (size) =>
-  [...Array(size)]
-    .map(() => Math.floor(Math.random() * 16).toString(16))
-    .join("");
-
-const mqttOptions = {
-  clean: true,
-  connectTimeout: 30 * 1000,
-  clientId: `React${genRanHex(6)}`,
-  username: `React${genRanHex(6)}`,
-  password: "emqx_test",
-};
+import { MqttContext } from "../waiting/MqttContextProvider";
 
 // TODO: doctor_id 적용
 const autoCallInfo = {
@@ -31,35 +15,9 @@ const autoCallInfo = {
 
 const doctor_id = 1;
 
-
 const DID = ({ nextState }) => {
-  const client = useRef();
+  const client = useContext(MqttContext);
   const [data, setData] = useState([]);
-  const autoCall = useRef(false);
-  const setAutoCall = (flag) => (autoCall.current = flag);
-  const [doctorFilter, setDoctorFilter] = useState({
-    1: true,
-    2: true,
-  });
-
-
-  const autoCallNext = useRef();
-  // const setAutoCallNext = (next) => (autoCallNext.current = next);
-  // useEffect(() => {
-  //   setAutoCallNext(data.find(autoCallInfo[initPanel]));
-  // }, [data]);
-
-  const callPatient = (reception_id) => {
-    console.log("callPatient", reception_id);
-    client.current.publish(
-      "waiting",
-      JSON.stringify({
-        method: "PUT",
-        data: { reception_id, state: nextState },
-      }),
-      { qos : 1 }
-    )
-  }
 
   // { method: "ADD | PUT | DELETE", data: { reception_id, ... }}
   const mqttListener = useCallback((topic, payload, packet) => {
@@ -89,12 +47,13 @@ const DID = ({ nextState }) => {
 
   }, []);
 
-
   useEffect(() => {
-    console.log("client init");
-    client.current = mqtt.connect(mqttURL, mqttOptions);
     client.current.subscribe(`waiting`, { qos: 1 });
     client.current.on("message", mqttListener);
+    return () => {
+      // client.current.unsubscribe(`waiting`);
+      // client.current.removeEvent(mqttListener);
+    }
   }, []);
 
   useEffect(() => {
