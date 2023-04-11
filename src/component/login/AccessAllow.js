d160cc6b406aebca74f5fa5f5fe4ef25066fbe63
jhkim <jinhankim988@gmail.com>
2023-04-11 11:04:50 +0900

    [Feat] refresh 토큰 이용 구현

--- src/component/login/AccessAllow.js
+++ src/component/login/AccessAllow.js
@@ -1,48 +1,66 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { refreshAccessToken } from "./AxiosClient";
+import { getLoginUserInfo } from './Login';
 
 const isIntersection = (arr1, arr2) => {
-    return arr1.some((v) => arr2.includes(v));
+  return arr1.some((v) => arr2.includes(v));
 };
 
 const check_token = () => {
-    const client_id = "client";
-    const client_secret = "secret";
-    const { access_token } = JSON.parse(localStorage.getItem("auth"));
-    return axios.post(`http://localhost:8081/oauth/check_token`, null, {
-        params: {token: access_token},
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-            'Authorization': `Basic ${btoa(`${client_id}:${client_secret}`)}`
-        }
-    });
+  const client_id = "client";
+  const client_secret = "secret";
+  let auth = JSON.parse(localStorage.getItem("auth"));
+  return axios
+  .post(`http://localhost:8081/oauth/check_token`, null, {
+    params: { token: auth.access_token },
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      Authorization: `Basic ${btoa(`${client_id}:${client_secret}`)}`,
+    },
+  });
 }
 
 const AccessAllow = ({ authorities, children }) => {
-    const [isChecked, setIsChecked] = useState(false);
-    const navigate = useNavigate();
-    const back = () => {
-    }
+  const [isChecked, setIsChecked] = useState(false);
+  const navigate = useNavigate();
+  const back = () => {
+    navigate(-1);
+  };
 
-    useEffect(() => {
-        const auth = JSON.parse(localStorage.getItem("auth"));
-        if (!auth) {
+  useEffect(() => {
+    const auth = JSON.parse(localStorage.getItem("auth"));
+    if (!auth) {
+      back();
+      setIsChecked(false);
+    } else if (isIntersection(auth.authorities, authorities)) {
+      const access_token_obj = JSON.parse(atob(auth.access_token.split(".")[1]));
+      if (access_token_obj.exp * 1000 < new Date().getTime()) {
+        refreshAccessToken().then(({ data }) => {
+          localStorage.setItem("auth", JSON.stringify({ ...auth, ...data }));
+          getLoginUserInfo();
+          check_token().then((response) => {
+            setIsChecked(true);
+          }).catch(error => {
             back();
             setIsChecked(false);
-        } else if (isIntersection(auth.authorities, authorities)) {
-            check_token().then(() => {
-                setIsChecked(true);
-            }).catch(err => {
-                back();
-                setIsChecked(false);
-            });
-        } else {
-            back();
-            setIsChecked(false);
-        }
-    }, [authorities]);
-    return <>{isChecked ? children : <></>}</>
-}
+          });
+        });
+      } else {
+        check_token().then((response) => {
+          setIsChecked(true);
+        }).catch(error => {
+          back();
+          setIsChecked(false);
+        });  
+      }
+    } else {
+      back();
+      setIsChecked(false);
+    }
+  }, [authorities]);
+  return <>{isChecked ? children : <></>}</>;
+};
 
 export default AccessAllow;
