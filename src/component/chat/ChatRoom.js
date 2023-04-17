0ceb1b10234746a6a9f0e39a89461bbc13176cc1
이정주 <wjdwn3132@naver.com>
2023-04-17 18:50:35 +0900

    [Feat] 채팅 구현

--- src/component/chat/ChatRoom.js
+++ src/component/chat/ChatRoom.js
@@ -163,151 +163,146 @@ const ChatRoom = ({ room, onBackClick }) => {
   };
 
   return (
-    <Box>
-      <Box sx={{ position: "absolute" }}>
-        {showEmojiList && <EmojiPicker onEmojiClick={handleEmojiClick} />}
-      </Box>
-      <Grid container spacing={1} sx={{ maxWidth: 400, maxHeight: 600 }}>
-        <Grid item xs={12}>
-          <AppBar
-            position="static"
-            style={{ borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
-          >
-            <Toolbar>
-              <Button color="inherit" onClick={onBackClick}>
-                <IconChevronLeft /> 목록 보기
-              </Button>
-            </Toolbar>
-          </AppBar>
-        </Grid>
-        <Grid item xs={12}>
-          <Box
-            id="messages-box"
-            sx={{
-              display: "block",
-              height: "400px",
-              overflowY: "auto",
-            }}
-            onScroll={handleMessagesScroll}
-          >
-            {messages.map((message, index) =>
-              message.from !== participants_id ? ( // 수정
-                <Card
-                  key={index}
-                  sx={{
-                    display: "flex",
-                    justifyContent: "flex-start",
-                  }}
-                >
-                  <Avatar sx={{ alignSelf: "center", marginRight: 1 }}>
-                    <img
-                      src={
-                        thumbnail.find((t) => t.employ_id === message.from)
-                          ? `/api/chat/getthumbnail?thumbnail_image=${
-                              thumbnail.find(
-                                (t) => t.employ_id === message.from
-                              ).thumbnail_image
-                            }`
-                          : ""
-                      }
-                      alt="사진"
-                      style={{ width: "100%" }}
-                    />
-                  </Avatar>
-                  <Box sx={{ display: "flex", marginTop: 1, marginBottom: 1 }}>
-                    <Box sx={{ display: "flex", flexDirection: "column" }}>
-                      <Box>{message.employee_name}</Box>
-                      <Box sx={{ display: "flex" }}>
-                        <Paper elevation={2}>
-                          <Box sx={{ padding: 1 }}>{message.message}</Box>
-                        </Paper>
-                        <Box
-                          sx={{
-                            fontSize: "12px",
-                            display: "flex",
-                            alignItems: "flex-end",
-                            marginLeft: 1,
-                          }}
-                        >
-                          {moment(message.created_at).format("a hh:mm")}
-                        </Box>
+    // <Box>
+    //   <Box sx={{ position: "absolute" }}>
+    //     {showEmojiList && <EmojiPicker onEmojiClick={handleEmojiClick} />}
+    //   </Box>
+    <Grid container spacing={1} sx={{ maxWidth: 400, maxHeight: 600 }}>
+      <Grid item xs={12}>
+        <AppBar
+          position="static"
+          style={{ borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
+        >
+          <Toolbar>
+            <Button color="inherit" onClick={onBackClick}>
+              <IconChevronLeft /> 목록 보기
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+      <Grid item xs={12}>
+        <Box
+          id="messages-box"
+          sx={{
+            display: "block",
+            height: "400px",
+            overflowY: "auto",
+          }}
+          onScroll={handleMessagesScroll}
+        >
+          {messages.map((message, index) =>
+            message.from !== participants_id ? ( // 수정
+              <Card
+                key={index}
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                }}
+              >
+                <Avatar sx={{ alignSelf: "center", marginRight: 1 }}>
+                  <img
+                    src={
+                      thumbnail.find((t) => t.employ_id === message.from)
+                        ? `/api/chat/getthumbnail?thumbnail_image=${
+                            thumbnail.find((t) => t.employ_id === message.from)
+                              .thumbnail_image
+                          }`
+                        : ""
+                    }
+                    alt="사진"
+                    style={{ width: "100%" }}
+                  />
+                </Avatar>
+                <Box sx={{ display: "flex", marginTop: 1, marginBottom: 1 }}>
+                  <Box sx={{ display: "flex", flexDirection: "column" }}>
+                    <Box>{message.employee_name}</Box>
+                    <Box sx={{ display: "flex" }}>
+                      <Paper elevation={2}>
+                        <Box sx={{ padding: 1 }}>{message.message}</Box>
+                      </Paper>
+                      <Box
+                        sx={{
+                          fontSize: "12px",
+                          display: "flex",
+                          alignItems: "flex-end",
+                          marginLeft: 1,
+                        }}
+                      >
+                        {moment(message.created_at).format("a hh:mm")}
                       </Box>
                     </Box>
                   </Box>
-                </Card>
-              ) : (
-                <Card
-                  key={index}
-                  sx={{
-                    display: "flex",
-                    justifyContent: "flex-end",
-                  }}
-                >
-                  <Box sx={{ display: "flex", marginTop: 1, marginBottom: 1 }}>
-                    <Box
-                      sx={{
-                        fontSize: "12px",
-                        display: "flex",
-                        alignItems: "flex-end",
-                      }}
-                    >
-                      {moment(message.created_at).format("a hh:mm")}
-                    </Box>
-                    <Paper
-                      elevation={2}
-                      sx={{ marginLeft: 1, backgroundColor: "#ffea00" }}
-                    >
-                      <Box sx={{ padding: 1 }}>{message.message}</Box>
-                    </Paper>
-                  </Box>
-                </Card>
-              )
-            )}
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Box
-            sx={{ display: "flex", justifyContent: "space-between", margin: 1 }}
-          >
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <IconSquarePlus
-                style={{ color: "#00aaff", width: "40px", height: "40px" }}
-                onClick={handleIconClick}
-              />
-            </Box>
-            <TextField
-              multiline
-              maxRows={4}
-              placeholder="내용을 입력해주세요."
-              value={message.message}
-              onChange={(e) =>
-                setMessage((prevMessage) => ({
-                  ...prevMessage,
-                  message: e.target.value,
-                  created_at: new Date().toISOString(),
-                }))
-              }
-              onKeyDown={(e) => {
-                if (e.key === "Enter" && !isDisabled) {
-                  e.preventDefault();
-                  publish();
-                }
-              }}
-              sx={{ width: "100%", marginLeft: 1, marginRight: 1 }}
-            />
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <Button
-                variant="contained"
-                onClick={publish}
-                disabled={isDisabled}
+                </Box>
+              </Card>
+            ) : (
+              <Card
+                key={index}
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
               >
-                <IconSend />
-              </Button>
-            </Box>
-          </Box>
-        </Grid>
+                <Box sx={{ display: "flex", marginTop: 1, marginBottom: 1 }}>
+                  <Box
+                    sx={{
+                      fontSize: "12px",
+                      display: "flex",
+                      alignItems: "flex-end",
+                    }}
+                  >
+                    {moment(message.created_at).format("a hh:mm")}
+                  </Box>
+                  <Paper
+                    elevation={2}
+                    sx={{ marginLeft: 1, backgroundColor: "#ffea00" }}
+                  >
+                    <Box sx={{ padding: 1 }}>{message.message}</Box>
+                  </Paper>
+                </Box>
+              </Card>
+            )
+          )}
+        </Box>
       </Grid>
-    </Box>
+      <Grid item xs={12}>
+        <Box
+          sx={{ display: "flex", justifyContent: "space-between", margin: 1 }}
+        >
+          {/* <Box sx={{ display: "flex", alignItems: "center" }}>
+            <IconSquarePlus
+              style={{ color: "#00aaff", width: "40px", height: "40px" }}
+              onClick={handleIconClick}
+            />
+          </Box> */}
+          <TextField
+            multiline
+            maxRows={4}
+            placeholder="내용을 입력해주세요."
+            value={message.message}
+            onChange={(e) =>
+              setMessage((prevMessage) => ({
+                ...prevMessage,
+                message: e.target.value,
+                created_at: new Date().toISOString(),
+              }))
+            }
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !isDisabled) {
+                e.preventDefault();
+                publish();
+              }
+            }}
+            sx={{ width: "100%", marginLeft: 1, marginRight: 1 }}
+          />
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Button variant="contained" onClick={publish} disabled={isDisabled}>
+              <IconSend />
+            </Button>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+    // </Box>
   );
 };
 
